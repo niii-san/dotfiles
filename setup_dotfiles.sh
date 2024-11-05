629e0c85b21daa0f4d45cd52e29d6653cc99b27a
Nishan Bista <nishanbista525@gmail.com>
2024-11-06 00:51:38 +0545

    dotfiles setup script

---
+++ setup_dotfiles.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+
+ln -s ~/dotfiles/btop ~/.config/btop
+ln -s ~/dotfiles/cava ~/.config/cava
+ln -s ~/dotfiles/dunst ~/.config/dunst
+ln -s ~/dotfiles/fastfetch ~/.config/fastfetch
+ln -s ~/dotfiles/hypr ~/.config/hypr
+ln -s ~/dotfiles/kitty ~/.config/kitty
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/rofi ~/.config/rofi
+ln -s ~/dotfiles/tmux ~/.config/tmux
+ln -s ~/dotfiles/waybar ~/.config/waybar
+ln -s ~/dotfiles/wlogout ~/.config/wlogout
+ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
+
+
+
+# Check if .bashrc exists in the home directory
+if [ -f "$HOME/.bashrc" ]; then
+    rm "$HOME/.bashrc"
+    echo "Removed existing .bashrc"
+
+fi
+
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+
+
+
+
+
+
+
+
