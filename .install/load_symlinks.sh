845d24e25aec9632d40c652c0f26fcdb7b47655b
Nishan Bista <nishanbista525@gmail.com>
2024-12-25 22:04:00 +0545

    screenshot script

--- .install/load_symlinks.sh
+++ .install/load_symlinks.sh
@@ -3,20 +3,50 @@
 
 echo "Loading Symbolic Links..."
 
+#btop
 ln -s ~/dotfiles/btop ~/.config/btop
+
+#cava
 ln -s ~/dotfiles/cava ~/.config/cava
+
+#dunst
 ln -s ~/dotfiles/dunst ~/.config/dunst
+
+#fastfetch
 ln -s ~/dotfiles/fastfetch ~/.config/fastfetch
+
+#hypr
 ln -s ~/dotfiles/hypr ~/.config/hypr
+
+#kitty
 ln -s ~/dotfiles/kitty ~/.config/kitty
+
+#neovim
 ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/rofi ~/.config/rofi  # rofi-wayland
+
+#rofi-wayland
+ln -s ~/dotfiles/rofi ~/.config/rofi
+
+#tmux
 ln -s ~/dotfiles/tmux ~/.config/tmux
+
+#waybar
 ln -s ~/dotfiles/waybar ~/.config/waybar
-ln -s ~/dotfiles/wlogout ~/.config/wlogout  # yay package
+
+#starship
 ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
 
+#swappy 
+ln -s ~/dotfiles/swappy ~/.config/swappy
 
+#yay packages -----------------------------------------------
+
+#wlogout
+ln -s ~/dotfiles/wlogout ~/.config/wlogout  # yay package
+
+
+
+# loading scripts
 ln -s ~/dotfiles/scripts ~/.config/scripts # custom scripts
 
 
