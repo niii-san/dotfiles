3f1c3686f9ee0e6eacf8df65ba348f57c4619bba
Nishan Bista <nishanbista525@gmail.com>
2025-02-22 23:46:16 +0545

    upgraded auto tag setup

--- nvim/lua/plugins/nvim-ts-autotag.lua
+++ nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,16 +1,20 @@
 return {
-	"windwp/nvim-ts-autotag",
-	opts = {
-		-- Defaults
-		enable_close = true, -- Auto close tags
-		enable_rename = true, -- Auto rename pairs of tags
-		enable_close_on_slash = true, -- Auto close on trailing </
+    "windwp/nvim-ts-autotag",
+    config = function()
+        local autotag = require("nvim-ts-autotag")
+        autotag.setup({
+            opts = {
+                -- Defaults
+                enable_close = true, -- Auto close tags
+                enable_rename = true, -- Auto rename pairs of tags
+                enable_close_on_slash = true, -- Auto close on trailing </
 
-		-- Override individual filetype configs
-		filetypes = {
-			html = {
-				enable_close = false, -- Disable auto-close for HTML
-			},
-		},
-	},
+                per_filetype = {
+                    ["html"] = {
+                        enable_close = false,
+                    },
+                },
+            },
+        })
+    end,
 }
