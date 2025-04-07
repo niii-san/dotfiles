a7043c4eb311175b8b16a364d63766ea46efd26e
Nishan Bista <lw.nishan@gmail.com>
2025-04-07 17:50:14 +0545

    new pluging nvim highlight color

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,59 +1,59 @@
 return {
-    {
-        "hrsh7th/cmp-nvim-lsp",
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-            "onsails/lspkind-nvim",
-        },
-        config = function()
-            -- Load required modules
-            local cmp = require("cmp")
-            local lspkind = require("lspkind")
-            local luasnip = require("luasnip")
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind-nvim",
+		},
+		config = function()
+			-- Load required modules
+			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+			local luasnip = require("luasnip")
 
-            -- Lazy load VSCode snippets
-            require("luasnip.loaders.from_vscode").lazy_load()
+			-- Lazy load VSCode snippets
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-            -- Setup nvim-cmp
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                }, {
-                    { name = "buffer", keyword_length = 3 },
-                }),
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = "symbol_text", -- Adds text to symbols
-                        maxwidth = 50,
-                        ellipsis_char = "...",
-                        before = function(_, vim_item)
-                            return vim_item
-                        end,
-                    }),
-                },
-            })
-        end,
-    },
+			-- Setup nvim-cmp
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer", keyword_length = 3 },
+				}),
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol_text", -- Adds text to symbols
+						maxwidth = 50,
+						ellipsis_char = "...",
+						before = function(_, vim_item)
+							return vim_item
+						end,
+					}),
+				},
+			})
+		end,
+	},
 }
