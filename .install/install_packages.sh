e867c7933648b98e7ccafe5c541a8066398f2a9f
Nishan Bista <lw.nishan@gmail.com>
2025-06-12 20:03:58 +0545

    somechanges

--- .install/install_packages.sh
+++ .install/install_packages.sh
@@ -7,7 +7,8 @@ read -r user_input
 user_input="${user_input//[[:space:]]/}"
 
 # Install Packages
-packages=("hyprland" "hyprpaper" "hypridle" "btop" "dunst" "fastfetch" "kitty" "neovim" "rofi-wayland" "tmux" "waybar" "starship")
+packages=("hyprland" "hyprpaper" "hypridle" "hyprlock" "thunar" "gvfs" "thunar-archive-plugin" "thunar-media-tags-plugin" 
+    "btop" "dunst" "fastfetch" "kitty" "neovim" "rofi-wayland" "tmux" "waybar" "starship" "yazi" "cava" "pavucontrol" "tumbler" "ffmpegthumbnailer" "grim" "slurp" "swappy" "imagemagick")
 
 if [[ "${user_input,,}" == "y" ]]; then
     echo "Installing packages........"
